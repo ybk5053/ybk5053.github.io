43dce072f5ffcb92238837d664ae10362b1e590d
ybk5053 <28558154+ybk5053@users.noreply.github.com>
2025-06-21 17:18:23 +0800

    Update gui.js

--- maze/gui.js
+++ maze/gui.js
@@ -58,7 +58,7 @@ class GUI {
         this.ctx.fillStyle = "white"
         this.ctx.fillRect(0,0,this.ctx.canvas.width, this.ctx.canvas.height)
         var width = this.canvas.width
-        var polygons = this.draw_layer(this.maze.player.position(), this.maze.player.face, width, 0.95, 1.0, 0.05, 0.95, 1.0, 0.0, 0.5)
+        var polygons = this.draw_layer(this.maze.player.position(), this.maze.player.face, width, 0.95, 1.0, 0.025, 0.975, 1.0, 0.0, 0.5)
         polygons.sort((a,b) => {
             return a.lt(b)
         })
@@ -79,6 +79,9 @@ class GUI {
     }
 
     draw_layer(position, face, width, floory, neary, startx, endx, rightx, leftx, limit, ignore='', layer=0, side=0) {
+        let ystep = 0.1
+        let ywallthick = 0.01
+        let xwallthick = 0.005
         if (floory <= 0 || floory > 1 || neary < 0 || neary > 1) {
             return []
         }
@@ -117,15 +120,15 @@ class GUI {
         if (this.maze.get_cell(position)[face]) {
             polygon.push(new Polygon([width*startx, 0, width*endx, 0, width*endx, width*floory, width*startx, width*floory], u, layer, side))
             if (ignore != '') {
-                polygon.push(new Polygon([width*startx, 0, width*(startx-0.01), 0, width*(startx-0.01), width*(floory-0.01), width*startx, width*floory], r, layer, side))
-                polygon.push(new Polygon([width*endx, 0, width*(endx+0.01), 0, width*(endx+0.01), width*(floory-0.01), width*endx, width*floory], l, layer, side))
+                polygon.push(new Polygon([width*startx, 0, width*(startx-xwallthick), 0, width*(startx-xwallthick), width*(floory-ywallthick), width*startx, width*floory], r, layer, side))
+                polygon.push(new Polygon([width*endx, 0, width*(endx+xwallthick), 0, width*(endx+xwallthick), width*(floory-ywallthick), width*endx, width*floory], l, layer, side))
             }
         } else if (ignore == 'r') {
-            polygon.push(...this.draw_layer(this.maze.get_adj_cell_pos(face,position), face, width, floory-0.05, floory, startx+0.05, endx+0.05, endx+0.05, startx, limit, ignore, layer+1, side))
+            polygon.push(...this.draw_layer(this.maze.get_adj_cell_pos(face,position), face, width, floory-ystep, floory, startx+0.05, endx+0.05, endx, startx, limit, ignore, layer+1, side))
         } else if (ignore == 'l') {
-            polygon.push(...this.draw_layer(this.maze.get_adj_cell_pos(face,position), face, width, floory-0.05, floory, startx, endx-(endx-startx)/4, endx, startx, limit, ignore, layer+1, side))
+            polygon.push(...this.draw_layer(this.maze.get_adj_cell_pos(face,position), face, width, floory-ystep, floory, startx-0.05, endx-0.05, endx, startx, limit, ignore, layer+1, side))
         } else {
-            polygon.push(...this.draw_layer(this.maze.get_adj_cell_pos(face,position), face, width, floory-0.05, floory, startx+0.05, endx-0.05, endx, startx, limit, ignore, layer+1, side))
+            polygon.push(...this.draw_layer(this.maze.get_adj_cell_pos(face,position), face, width, floory-ystep, floory, startx+0.05, endx-0.05, endx, startx, limit, ignore, layer+1, side))
         }
         if (ignore != 'l') {
             if (this.maze.get_cell(position)[left(face)]) {
