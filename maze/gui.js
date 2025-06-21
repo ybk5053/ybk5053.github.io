2f39b5b6854c7c64e116c2d7cff80b0cf410aa41
ybk5053 <28558154+ybk5053@users.noreply.github.com>
2025-06-21 20:34:16 +0800

    Update gui.js

--- maze/gui.js
+++ maze/gui.js
@@ -28,8 +28,6 @@ class GUI {
 
         this.canvas = document.getElementById("viewcanvas")
         this.ctx = this.canvas.getContext("2d")
-        var wall = new Image();
-        wall.src = "wall.jpg";
         let canvas1 = document.createElement( 'canvas' );
         let ctx1 = canvas1.getContext( '2d' );
         canvas1.width = 64
